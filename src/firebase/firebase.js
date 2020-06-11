a7f8ca66e09da2136a446ab7e99d417e78ed8668
sammacfarlane23 <50931982+sammacfarlane23@users.noreply.github.com>
2020-06-11 11:14:09 +0100

    Hardcoded Firebase API details instead of environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  apiKey: 'AIzaSyDlCtVoisoAJ6B5FgaU8vOmypOr1woSZJQ',
+  authDomain: 'journal-1a878.firebaseapp.com',
   databaseURL: 'https://journal-1a878.firebaseio.com',
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  projectId: 'journal-1a878',
+  storageBucket: '1a878.appspot.com',
+  messagingSenderId: '854931825276',
+  appId: '1:854931825276:web:9d5bd4ed5df1a5ec749ff9',
+  measurementId: 'G-4FLQF7ZFZ6',
 };
 
 // Initialize Firebase
