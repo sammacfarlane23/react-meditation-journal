6ece39316a42b9b0313a41f24e055e755b0bce87
sammacfarlane23 <50931982+sammacfarlane23@users.noreply.github.com>
2020-06-15 12:46:42 +0100

    Added Facebook login feature

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import { startGoogleLogin, startFacebookLogin } from '../actions/auth';
 
-export const LoginPage = ({ startLogin }) => (
+export const LoginPage = ({ startGoogleLogin, startFacebookLogin }) => (
   <div className='box-layout'>
     <div className='box-layout__box'>
       <h1 className='box-layout__title'>Meditation Journal</h1>
       <p>A simple app to journal your experience of meditation</p>
-      <button className='button button--login' onClick={startLogin}>
-        Login with Google
-      </button>
+      <div className='login-button-container'>
+        <button
+          className='button button--login-google'
+          onClick={startGoogleLogin}
+        >
+          Login with Google
+        </button>
+        <button
+          className='button button--login-facebook'
+          onClick={startFacebookLogin}
+        >
+          Login with Facebook
+        </button>
+      </div>
     </div>
   </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-  startLogin: () => dispatch(startLogin()),
+  startGoogleLogin: () => dispatch(startGoogleLogin()),
+  startFacebookLogin: () => dispatch(startFacebookLogin()),
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,5 +16,11 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+var facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
 
-export { firebase, googleAuthProvider, database as default };
+export {
+  firebase,
+  googleAuthProvider,
+  facebookAuthProvider,
+  database as default,
+};
