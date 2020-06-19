4eff8db381aaf1d2409d5d6cb422d28cb310c76b
sammacfarlane23 <50931982+sammacfarlane23@users.noreply.github.com>
2020-06-19 11:06:25 +0100

    Added animations

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -4,36 +4,32 @@ import { Spring } from 'react-spring/renderprops';
 import EntryItem from './EntryItem';
 import selectEntries from '../selectors/entries';
 
-export class EntryList extends React.Component {
-  state = { show: true };
-
-  render() {
-    return (
-      <div className='entry-list'>
-        {!this.props.hasEntries ? (
-          <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-            {(props) => (
-              <div style={props}>
-                <div className='entry-item entry-item--message'>
-                  Welcome to Meditation Journal App. Click the plus button below
-                  to add an entry.
-                </div>
+export const EntryList = (props) => {
+  return (
+    <div className='entry-list'>
+      {!props.hasEntries ? (
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+          {(props) => (
+            <div style={props}>
+              <div className='entry-item entry-item--message'>
+                Welcome to Meditation Journal App. Click the plus button below
+                to add an entry.
               </div>
-            )}
-          </Spring>
-        ) : this.props.entries.length === 0 ? (
-          <div className='entry-item entry-item--message'>
-            <span>No matching entries :(</span>
-          </div>
-        ) : (
-          this.props.entries.map((entry) => {
-            return <EntryItem key={entry.id} {...entry} />;
-          })
-        )}
-      </div>
-    );
-  }
-}
+            </div>
+          )}
+        </Spring>
+      ) : props.entries.length === 0 ? (
+        <div className='entry-item entry-item--message'>
+          <span>No matching entries :(</span>
+        </div>
+      ) : (
+        props.entries.map((entry) => {
+          return <EntryItem key={entry.id} {...entry} />;
+        })
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
