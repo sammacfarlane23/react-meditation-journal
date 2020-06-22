48be9e99ec4f71aa9f9c2a5593cc52862c0d90d7
sammacfarlane23 <50931982+sammacfarlane23@users.noreply.github.com>
2020-06-22 11:45:37 +0100

    Revert "Added some basic animations"
    This reverts commit 968a389b62a2e1761673ce7cb3bdf5b08cf2ef13.
    
    Reverting back to last working commit

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -3,28 +3,24 @@ import { connect } from 'react-redux';
 import EntryItem from './EntryItem';
 import selectEntries from '../selectors/entries';
 
-export const EntryList = (props) => {
-  return (
-    <div className='entry-list'>
-      {!props.hasEntries ? (
-        <div style={props}>
-          <div className='entry-item entry-item--message'>
-            Welcome to Meditation Journal App. Click the plus button below to
-            add an entry.
-          </div>
-        </div>
-      ) : props.entries.length === 0 ? (
-        <div className='entry-item entry-item--message'>
-          <span>No matching entries :(</span>
-        </div>
-      ) : (
-        props.entries.map((entry) => {
-          return <EntryItem key={entry.id} {...entry} />;
-        })
-      )}
-    </div>
-  );
-};
+export const EntryList = (props) => (
+  <div className='entry-list'>
+    {!props.hasEntries ? (
+      <div className='entry-item entry-item--message'>
+        Welcome to Meditation Journal App. Click the plus button below to add an
+        entry.
+      </div>
+    ) : props.entries.length === 0 ? (
+      <div className='entry-item entry-item--message'>
+        <span>No matching entries :(</span>
+      </div>
+    ) : (
+      props.entries.map((entry) => {
+        return <EntryItem key={entry.id} {...entry} />;
+      })
+    )}
+  </div>
+);
 
 const mapStateToProps = (state) => {
   return {
