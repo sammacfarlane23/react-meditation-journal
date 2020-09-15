712fa20e5917aac1e368c01406e7f9e84ebf5cc5
sammacfarlane23 <50931982+sammacfarlane23@users.noreply.github.com>
2020-09-15 11:35:18 +0100

    Undid last change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import SearchIcon from './SearchIcon';
 import EntryListFilters from './EntryListFilters';
