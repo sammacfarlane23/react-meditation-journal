649f90d290a666d516556c64ac4153a5a15c4c0e
sammacfarlane23 <50931982+sammacfarlane23@users.noreply.github.com>
2020-09-30 16:26:54 +0100

    Converted AddEntryButton from class component to function component

--- server/server.js
+++ server/server.js
@@ -13,5 +13,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server is up!');
+  console.log('Server is up! Running on port ' + port);
 });
--- src/components/AddEntryButton.js
+++ src/components/AddEntryButton.js
@@ -1,45 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import EntryModal from './EntryModal';
 import { startCreateEntry } from '../actions/entries';
 
-export class AddEntryButton extends React.Component {
-  state = { show: false };
+export const AddEntryButton = (props) => {
+  const [show, setShow] = useState(false);
 
-  showModal = () => {
-    this.setState(() => ({ show: true }));
+  const showModal = () => {
+    setShow(true);
   };
 
-  onSubmit = (entry) => {
-    this.props.startCreateEntry(entry);
-    this.setState({ show: false });
+  const onSubmit = (entry) => {
+    props.startCreateEntry(entry);
+    setShow(false);
   };
 
-  closeModal = () => {
-    this.setState({ show: false });
+  const closeModal = () => {
+    setShow(false);
   };
-  render() {
-    return (
-      <div className='button-position'>
-        {!this.state.show && (
-          <button
-            className='button--big circular-container'
-            onClick={this.showModal}
-          >
-            <FontAwesomeIcon icon={faPlus} size='2x' />
-          </button>
-        )}
-        <EntryModal
-          showModal={this.state.show}
-          onSubmit={this.onSubmit}
-          closeModal={this.closeModal}
-        />
-      </div>
-    );
-  }
-}
+
+  return (
+    <div className='button-position'>
+      {!show && (
+        <button className='button--big circular-container' onClick={showModal}>
+          <FontAwesomeIcon icon={faPlus} size='2x' />
+        </button>
+      )}
+      <EntryModal
+        showModal={show}
+        onSubmit={onSubmit}
+        closeModal={closeModal}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   startCreateEntry: (entry) => dispatch(startCreateEntry(entry)),
