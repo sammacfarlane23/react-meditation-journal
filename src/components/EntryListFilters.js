7cbef4599f427f3329bbd056d30176e0b832a274
sammacfarlane23 <50931982+sammacfarlane23@users.noreply.github.com>
2020-10-02 12:21:20 +0100

    Changed EntryListFilters to functional component

--- src/components/EntryListFilters.js
+++ src/components/EntryListFilters.js
@@ -2,26 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setTextFilter } from '../actions/filters';
 
-export class EntryListFilters extends React.Component {
-  onTextChange = (e) => {
-    this.props.setTextFilter(e.target.value);
+export const EntryListFilters = (props) => {
+  const onTextChange = (e) => {
+    props.setTextFilter(e.target.value);
   };
 
-  render() {
-    return (
-      <div>
-        <input
-          type='text'
-          autoFocus
-          placeholder='Search entries'
-          value={this.props.filters.text}
-          onChange={this.onTextChange}
-          className='search-bar'
-        ></input>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <input
+        type='text'
+        autoFocus
+        placeholder='Search entries'
+        value={props.filters.text}
+        onChange={onTextChange}
+        className='search-bar'
+      ></input>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   filters: state.filters,
