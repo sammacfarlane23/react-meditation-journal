e8fae6c48902a0bfaa0a577977702cc642d5fa56
Sam MacFarlane <sam@Sams-MacBook-Air.local>
2021-05-20 19:20:38 +0100

    refactor: Firebase vars in .env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyDlCtVoisoAJ6B5FgaU8vOmypOr1woSZJQ',
-  authDomain: 'journal-1a878.firebaseapp.com',
-  databaseURL: 'https://journal-1a878.firebaseio.com',
-  projectId: 'journal-1a878',
-  storageBucket: '1a878.appspot.com',
-  messagingSenderId: '854931825276',
-  appId: '1:854931825276:web:9d5bd4ed5df1a5ec749ff9',
-  measurementId: 'G-4FLQF7ZFZ6',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
